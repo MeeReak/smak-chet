ed11e0bca37bdb3f33998345886688b1bf28140d
Chan Sarun <chansarun0@gmail.com>
2024-01-30 16:03:22 +0700

    Button Component

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+'use client'
+import React, { FC, ReactNode } from 'react';
+import Image from 'next/image';
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
+interface ButtonProps{
+    children : ReactNode;
+    Icon ?: string; 
+    className ?: string;
+    colorScheme ?: "primary" | "secondary"
+}
+const Button: FC<ButtonProps> = ({
+    children,
+    Icon,
+    className = "",
+    colorScheme = "primary",
+})=>{
+    const getColorSchemeClass = (scheme: string) => {
+        switch (scheme) {
+          case "primary":
+            return "bg-[#207BFF] hover:bg-[#BCD7FF]"; 
+          case "secondary":
+            return "bg-[#FF2020]";
+          default:
+            return "bg-[#207BFF] hover:bg-[rgba(32, 123, 255, 0.30)]";
+        }
+    }
+    const colorSchemeClass = getColorSchemeClass(colorScheme);
+    const combinedClassName = `text-center ${colorSchemeClass} ${className}`;
+
+    return (
+        <button className={combinedClassName}>
+            <img src={Icon} alt="" />
+          {children}
+        </button>
+      );
 }
 
-export  {Button}
+export {Button};
