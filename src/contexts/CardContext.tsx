b732472087efdc139fba31df9d49f06bfda024f7
Mee Reak <138286828+MeeReak@users.noreply.github.com>
2024-03-01 11:01:22 +0700

    update data in context

--- src/contexts/CardContext.tsx
+++ src/contexts/CardContext.tsx
@@ -1,99 +1,87 @@
-"use client"
+"use client";
 
-import React , {createContext , useState ,ReactNode} from 'react'
+import React, { createContext, useState, ReactNode } from "react";
 
-interface CardModal{
-    src: string,
-    alt: string,
-    title : string,
-    description : string,
-    date : string,
-    location : string
+interface CardModal {
+  src: string;
+  alt: string;
+  title: string;
+  description: string;
+  date: string;
+  location: string;
 }
 
-interface CardContextProp{
-    children : ReactNode
+interface CardContextProp {
+  children: ReactNode;
 }
 
-interface ContextProps{
-    CardInfo : CardModal[]
+interface ContextProps {
+  CardInfo: CardModal[];
 }
 
-export const MyContext = createContext<ContextProps>(
+export const MyContext = createContext<ContextProps>({
+  CardInfo: [],
+});
+
+const CardContext: React.FC<CardContextProp> = ({ children }) => {
+  const [CardInfo, setCardInfo] = useState([
     {
-        CardInfo : []
-    }
-);
+      id: "1",
+      src: "/assets/image/National_day_of_Science,Technology_and_Innovation.png",
+      alt: "National day of Science, Technology and Innovation 2024",
+      title: "National day of Science, Technology and Innovation 2024",
+      date: "Mar 03 2024",
+      location: "Phnom Penh",
+    },
+    {
+      id: "2",
+      src: "/assets/image/ace_volunteer.png",
+      alt: "Cambodia Book Fair",
+      title: "Cambodia Book Fair",
+      date: "Oct 25 2024",
+      location: "Phnom Penh",
+    },
+    {
+      id: "3",
+      src: "/assets/image/ace_volunteer.png",
+      alt: "ACE Graduation Ceremony",
+      title: "ACE Graduation Ceremony",
+      date: "May 18 2024",
+      location: "Phnom Penh",
+    },
+    {
+      id: "4",
+      src: "/assets/image/National_day_of_Science,Technology_and_Innovation.png",
+      alt: "National day of Science, Technology and Innovation 2024",
+      title: "National day of Science, Technology and Innovation 2024",
+      date: "Mar 03 2024",
+      location: "Phnom Penh",
+    },
+    {
+      id: "5",
+      src: "/assets/image/ace_volunteer.png",
+      alt: "Cambodia Book Fair",
+      title: "Cambodia Book Fair",
+      date: "Oct 25 2024",
+      location: "Phnom Penh",
+    },
+    {
+      id: "6",
+      src: "/assets/image/ace_volunteer.png",
+      alt: "ACE Graduation Ceremony",
+      title: "ACE Graduation Ceremony",
+      date: "May 18 2024",
+      location: "Phnom Penh",
+    },
+  ]);
 
-
-const CardContext:React.FC<CardContextProp> = ({children}) => {
-
-    const [CardInfo , setCardInfo] = useState([
-        {
-          src: "/assets/image/cambodiaBookFair.svg",
-          alt: "Cambodia Book Fair",
-          title: "Cambodia Book Fair",
-          description:
-            "Grab a chance to be this special National Event's Volunteer...",
-          date: "Dec 12 2024",
-          location: "Phnom Penh",
-        },
-        {
-          src: "/assets/image/volunteeer.svg",
-          alt: "Cambodia Book Fair",
-          title: "Cambodia Book Fair",
-          description:
-            "Grab a chance to be this special National Event's Volunteer...",
-          date: "Dec 12 2024",
-          location: "Phnom Penh",
-        },
-        {
-          src: "/assets/image/international-camp.svg",
-          alt: "Cambodia Book Fair",
-          title: "Cambodia Book Fair",
-          description:
-            "Grab a chance to be this special National Event's Volunteer...",
-          date: "Dec 12 2024",
-          location: "Phnom Penh",
-        },
-        {
-          src: "/assets/image/cambodiaBookFair.svg",
-          alt: "Cambodia Book Fair",
-          title: "Cambodia Book Fair",
-          description:
-            "Grab a chance to be this special National Event's Volunteer...",
-          date: "Dec 12 2024",
-          location: "Phnom Penh",
-        },
-        {
-          src: "/assets/image/volunteeer.svg",
-          alt: "Cambodia Book Fair",
-          title: "Cambodia Book Fair",
-          description:
-            "Grab a chance to be this special National Event's Volunteer...",
-          date: "Dec 12 2024",
-          location: "Phnom Penh",
-        },
-        {
-          src: "/assets/image/international-camp.svg",
-          alt: "Cambodia Book Fair",
-          title: "Cambodia Book Fair",
-          description:
-            "Grab a chance to be this special National Event's Volunteer...",
-          date: "Dec 12 2024",
-          location: "Phnom Penh",
-        },
-      ]);
-
-      const Contextvalue = {
-        CardInfo,
-        setCardInfo
-      }
+  const Contextvalue = {
+    CardInfo,
+    setCardInfo,
+  };
   return (
-    <MyContext.Provider value={Contextvalue}>
-        {children}
-    </MyContext.Provider> 
-  )
-}
+    <MyContext.Provider value={Contextvalue}>{children}</MyContext.Provider>
+  );
+};
 
-export default CardContext
+export default CardContext;
