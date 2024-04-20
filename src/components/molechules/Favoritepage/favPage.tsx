4bb29554afd3be1e3fe3d5b970be9761d4e8ae6f
Mee Reak <138286828+MeeReak@users.noreply.github.com>
2024-04-20 16:41:41 +0700

    add new page and responsive favpage

--- src/components/molechules/Favoritepage/favPage.tsx
+++ src/components/molechules/Favoritepage/favPage.tsx
@@ -1,13 +1,13 @@
 "use client";
-import { CardModal, MyContext } from "@/contexts/CardContext";
-import React, { useContext, useState } from "react";
+import { MyContext } from "@/contexts/CardContext";
+import React, { useContext } from "react";
 import { Card } from "../Card";
 
 const FavPage = () => {
   const { CardInfo } = useContext(MyContext);
   return (
     <>
-      <div className="grid xl:grid-cols-3 gap-[22px] justify-items-center">
+      <div className="max-[1030px]:px-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">
         {CardInfo.filter((item) => item.isFavorite).map((item, index) => (
           <Card
             key={item.id} // Assuming `item.id` is unique; otherwise, you can fallback to index
