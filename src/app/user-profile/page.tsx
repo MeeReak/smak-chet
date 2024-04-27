3f9098f33fd3ccd346a847bc619d1b1e7fe4883b
Mee Reak <138286828+MeeReak@users.noreply.github.com>
2024-04-27 14:53:45 +0700

    update users profile page

--- src/app/user-profile/page.tsx
+++ src/app/user-profile/page.tsx
@@ -1,86 +1,119 @@
-
 import React from "react";
 import Image from "next/image";
-import {
-  Button,
-  ButtonIcon,
-  InputData,
-  InputSearch,
-  Typography,
-} from "@/components";
+import { Button, ButtonIcon, InputData, Typography } from "@/components";
 
 const page = () => {
   return (
-    <div className=" mt-[141px] xl:mx-[273px] md:mx-5 sm:mx-2"> {/* Base container */}
-      {/* Profile and Button Section */}
-      <div className="flex flex-col items-center gap-4 md:flex-row"> {/* Responsive row */}
-        <div className="profile-image w-32 h-32 overflow-hidden md:w-48 md:h-48"> {/* Responsive image */}
-          <Image
-            src={"/assets/image/profile.png"}
-            alt={"profile image"}
-            width={160}
-            height={160}
+    <>
+      <div className="w-[885px] mt-20 mx-auto ">
+        <Typography fontSize="h3" fontWeight="semibold" className="p-5">
+          User Infomation
+        </Typography>
+        <div className=" flex gap-x-14">
+          {/* Profile Picture */}
+          <div className="relative w-[200px] h-[200px]  ">
+            <Image
+              className=" object-cover w-[200px] h-[200px] rounded-[10px]"
+              src="/assets/image/leap.jpg"
+              alt="logo"
+              width={200}
+              height={150}
+            />
+            <ButtonIcon
+              className="absolute -bottom-2 -right-2 bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
+              icon={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                  />
+                </svg>
+              }
+            />
+          </div>
+          {/* Information User */}
+          <div className=" grid grid-cols-2 gap-5">
+            <div>
+              <Typography className="text-gray-500 pb-2" fontSize="h4">
+                {/* Name */}
+                Full Name
+              </Typography>
+              <InputData
+                type="text"
+                defaultValue="Peng Maleap"
+                placeholder={"Enter your fullname"}
+                className="py-4 pl-4 pr-[70px] border text-base border-gray-200 bg-gray-100  mb-2 font-semibold"
+              />
+            </div>
+            <div>
+              {/* Email */}
+              <Typography className="text-gray-500 pb-2" fontSize="h4">
+                Email
+              </Typography>
+              <InputData
+                type="text"
+                defaultValue="pengmaleap456@gmail.com"
+                placeholder={"Enter yout email"}
+                className="py-4 pl-4 pr-[70px] border text-base border-gray-200 bg-gray-100 mb-2 font-semibold"
+              />
+            </div>
+            <div>
+              {/* Phone Number */}
+              <Typography className="text-gray-500 pb-2" fontSize="h4">
+                Phone Number
+              </Typography>
+              <InputData
+                type="text"
+                defaultValue="012 345 678"
+                placeholder={"Enter yout phone number"}
+                className="py-4 pl-4 pr-[70px] border text-base border-gray-200 bg-gray-100 mb-2 font-semibold"
+              />
+            </div>
+            <div>
+              {/* Address */}
+              <Typography className="text-gray-500 pb-2" fontSize="h4">
+                Address
+              </Typography>
+              <InputData
+                type="text"
+                defaultValue="Phnom Penh"
+                placeholder={"Enter your address"}
+                className="py-4 pl-4 pr-[70px] border text-base bg-gray-100 border-gray-200 mb-2 font-semibold"
+              />
+            </div>
+          </div>
+        </div>
+        <div>
+          {/* About user */}
+          <Typography className="text-gray-500 py-2" fontSize="h4">
+            About me
+          </Typography>
+          <textarea
+            name="about-user"
+            defaultValue={"This is me"}
+            className=" outline-none p-4 resize-none border border-gray-200 bg-gray-100 rounded-lg font-semibold"
+            cols={90}
+            rows={5}
           />
         </div>
-        <div className="flex flex-col items-center xl:items-start space-y-4"> {/* Responsive button section */}
-    
-           <Button
-              className="space-x-9 py-3 border-2 border-[#4EA5FF] w-64 rounded-[10px] "
-              leftIcon={
-                <Image
-                  src={"/assets/icons/refresh.svg"}
-                  width={30}
-                  height={30}
-                  alt={"profile image"}
-                />
-              }
-            >
-              <Typography fontSize="h3">Change Avatar</Typography>
-            </Button>
-           
-          <Typography fontSize="h3" className="text-gray-500">
-            Upload JPG, PNG image required
-          </Typography>
+        <div className="flex gap-3 justify-end mt-10">
+          <Button className="px-8 py-3" round="xl" colorScheme="primary">
+            Cancel
+          </Button>
+          <Button className="px-10 py-3" round="xl" bgColor="primary" colorScheme="White">
+            Save
+          </Button>
         </div>
       </div>
-
-      {/* Input Section */}
-      <div className="mt-8 mx-5 grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 "> {/* Responsive grid */}
-        <div>
-          <Typography>Full Name</Typography>
-          <InputData placeholder="" type="text" className="w-full border border-gray-300 rounded-md" />
-        </div>
-        <div>
-          <Typography>Address</Typography>
-          <InputData placeholder="" type="address" className="w-full border border-gray-300 rounded-md" />
-        </div>
-        <div>
-          <Typography>Email</Typography>
-          <InputData placeholder="" type="email" className="w-full border border-gray-300 rounded-md" />
-        </div>
-        <div>
-          <Typography>Phone Number</Typography>
-          <InputData placeholder="" type="text" className="w-full border border-gray-300 rounded-md" />
-        </div>
-        <div className="col-span-full"> {/* Full-width description */}
-          <Typography>Description</Typography>
-          <InputData placeholder=""  type="text" className="w-full h-32 border border-gray-300 rounded-md" />
-        </div>
-      </div>
-
-      {/* Upload section */}
-      <div className="flex justify-center mt-8 mx-24">
-       
-        <Button
-          bgColor="primary"
-          colorScheme="White"
-          size="h3"
-          className="xl:px-[165px] xl:py-[16px] px-[100px] py-[16px] justify-center"
-        >
-          Update
-        </Button>
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/components/molechules/UserProfileDropdown/UserProfileDropdown.tsx
+++ src/components/molechules/UserProfileDropdown/UserProfileDropdown.tsx
@@ -1,16 +1,26 @@
 "use client";
 import React, { useState } from "react";
-
-import { ButtonIcon } from "@/components/atoms";
+import { ButtonIcon, Typography } from "@/components/atoms";
 import Link from "next/link";
 
-const UserProfileDropdown = () => {
-  const [isOpen, setIsOpen] = useState(false);
+interface UserProfileDropdownProps {
+  isOpen: boolean;
+  setIsOpen: (value: boolean) => void;
+  setNotiOpen: (value: boolean) => void;
+}
 
+const UserProfileDropdown: React.FC<UserProfileDropdownProps> = ({
+  isOpen,
+  setIsOpen,
+  setNotiOpen,
+}) => {
   return (
-    <div className="relative">
+    <div>
       <ButtonIcon
-        onclick={() => setIsOpen(!isOpen)}
+        onclick={() => {
+          setIsOpen(!isOpen);
+          setNotiOpen(false);
+        }}
         className="ml-[10px] bg-gray-100 hover:bg-[#bdd8ff] text-white rounded-full p-2 max-[640px]:hidden"
         icon={
           <svg
@@ -29,87 +39,32 @@ const UserProfileDropdown = () => {
         }
       />
       {isOpen && (
-        <div className="pl-4 absolute right-[5px] mt-2 bg-white w-[270px] h-[162px] shadow-lg rounded-[10px] box-shadow: 1px -1px 5px 0px #00000040;">
-          <Link
-            className="flex justify-between items-center"
-            href={"/user-profile"}
-          >
-            My Profile
-            <ButtonIcon
-              icon={
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M19.6036 18.5832C19.6058 18.5871 19.6069 18.5897 19.6074 18.5912C19.607 18.593 19.6059 18.5967 19.603 18.6022C19.5911 18.6246 19.558 18.6587 19.4985 18.6712C14.5526 19.7019 9.44733 19.7019 4.50145 18.6712C4.4419 18.6587 4.40887 18.6246 4.39699 18.6022C4.39409 18.5967 4.39296 18.593 4.39254 18.5912C4.39308 18.5897 4.39419 18.5871 4.39636 18.5832C4.93 17.6247 5.78379 16.76 6.90548 16.0905C8.35155 15.2275 10.1426 14.75 12 14.75C13.8574 14.75 15.6484 15.2275 17.0945 16.0906C18.2162 16.76 19.07 17.6247 19.6036 18.5832ZM19.6079 18.5929C19.6078 18.5928 19.6078 18.5926 19.6077 18.5921L19.6079 18.5929ZM4.39209 18.5929C4.39207 18.5929 4.3921 18.5926 4.39225 18.5921L4.39209 18.5929Z"
-                    stroke="black"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                  />
-                  <circle
-                    cx="12"
-                    cy="8"
-                    r="4.25"
-                    stroke="black"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                  />
-                </svg>
-              }
-            />
-          </Link>
+        <div className="relative">
+          <div className="bg-white py-2 absolute w-[306px] flex flex-col right-0 mt-2 shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-[10px] ">
+            <Link href={"/user-profile"}>
+              <Typography className="p-3 hover:bg-gray-100" fontSize="h4">
+                My Profile
+              </Typography>
+            </Link>
 
-          <Link className="flex justify-between items-center" href={""}>
-            Languages
-            <ButtonIcon
-              icon={
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M10.5 21L15.75 9.75L21 21M12 18H19.5M3 5.621C4.9904 5.37332 6.99425 5.24941 9 5.25M9 5.25C10.12 5.25 11.233 5.288 12.334 5.364M9 5.25V3M12.334 5.364C11.176 10.658 7.69 15.08 3 17.502M12.334 5.364C13.23 5.425 14.119 5.511 15 5.621M10.411 14.116C8.77097 12.4486 7.47113 10.478 6.584 8.314"
-                    stroke="black"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              }
-            />
-          </Link>
+            <Link href={"/contact-us"}>
+              <Typography className="p-3 hover:bg-gray-100" fontSize="h4">
+                Contact Us
+              </Typography>
+            </Link>
 
-          <Link className="flex justify-between items-center" href={""}>
-            Log out
-            <ButtonIcon
-              icon={
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M2 12L1.41435 11.5315L1.03953 12L1.41435 12.4685L2 12ZM11 12.75C11.4142 12.75 11.75 12.4142 11.75 12C11.75 11.5858 11.4142 11.25 11 11.25V12.75ZM5.41435 6.53148L1.41435 11.5315L2.58565 12.4685L6.58565 7.46852L5.41435 6.53148ZM1.41435 12.4685L5.41435 17.4685L6.58565 16.5315L2.58565 11.5315L1.41435 12.4685ZM2 12.75H11V11.25H2V12.75Z"
-                    fill="black"
-                  />
-                  <path
-                    d="M10 8.13193V7.38851C10 5.77017 10 4.961 10.474 4.4015C10.9479 3.84201 11.7461 3.70899 13.3424 3.44293L15.0136 3.1644C18.2567 2.62388 19.8782 2.35363 20.9391 3.25232C22 4.15102 22 5.79493 22 9.08276V14.9172C22 18.2051 22 19.849 20.9391 20.7477C19.8782 21.6464 18.2567 21.3761 15.0136 20.8356L13.3424 20.5571C11.7461 20.291 10.9479 20.158 10.474 19.5985C10 19.039 10 18.2298 10 16.6115V16.066"
-                    stroke="black"
-                    stroke-width="1.5"
-                  />
-                </svg>
-              }
-            />
-          </Link>
+            <Link href={"/about-us"}>
+              <Typography className="p-3 hover:bg-gray-100" fontSize="h4">
+                About Us
+              </Typography>
+            </Link>
+
+            <Link href={""}>
+              <Typography className="p-3 hover:bg-gray-100" fontSize="h4">
+                Log out
+              </Typography>
+            </Link>
+          </div>
         </div>
       )}
     </div>
