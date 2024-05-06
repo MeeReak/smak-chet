1f712d8fe5430d3567245b505a09c459e4e6693f
pengmaleap <pengmaleap@gmail.com>
2024-05-06 09:02:28 +0700

    submitform

--- src/app/submitform/page.tsx
+++ src/app/submitform/page.tsx
@@ -1,13 +1,37 @@
-import SubmitForm from '@/components/organisms/submitForm/submitForm'
-import React from 'react'
+"use client"
+import SubmitForm, { SubmitFormProps } from '@/components/organisms/submitForm/submitForm'
+import React, { useState } from 'react'
 
-const page = () => {
+interface SubmitFormProp {
+  name: string;
+  address: string;
+  email: string;
+  phonenumber: string;
+}
 
+const Page = () => {
+  const [submitForm, setSubmitForm] = useState<SubmitFormProp>({
+    name: "",
+    address: "",
+    email: "",
+    phonenumber: ""   
+  })
+  
+   const handleOnChange = (name:string, address:string, email:string, phonenumber:string) => {
+     setSubmitForm((pre) => ({
+       ...pre,
+       name: name,
+       address: address,
+       email: email,
+       phonenumber: phonenumber,
+     }));
+     console.log(submitForm);
+   };
     return (
       <div className="xl:w-[1024px] w-screen m-auto space-y-5 z-10 mt-[100px] mb-20">
-        <SubmitForm />  
+        <SubmitForm onchange={(name, address, email, phonenumber)=>handleOnChange(name, address, email, phonenumber)} />  
       </div>
     );
 }
 
-export default page
+export default Page
