3142f65f1b6d7fde4869e6490bb98aba1bcdcf06
Chea_Pinglong <cheapinglong312@gmail.com>
2024-05-20 00:42:51 +0700

    add color for status text

--- src/app/applicantTable/page.tsx
+++ src/app/applicantTable/page.tsx
@@ -13,97 +13,7 @@ const Page = () => {
   console.log(totalCandidate);
 
   return (
-    // <div className="bg-[#F4FBFF]">
-    //   <div className="bg-white mx-auto">
-    //     <div className="w-[1024px] h-[159px] flex justify-between items-center mt-16 px-12 mx-auto">
-    //       <Typography fontWeight="semibold" fontSize="h2">
-    //         Table Of Volunteers
-    //       </Typography>
-    //       <div className="flex justify-center items-center gap-6 mt-12">
-    //         {/* table total */}
-    //         <div className="w-[220px] h-[70px] mb-14 rounded-[10px] border-[#E8E8E8] border-[0.5px] flex justify-center items-center gap-2 shadow-md">
-    //           <Typography color="blue" fontSize="h3" fontWeight="medium">
-    //             Total :
-    //           </Typography>
-    //           <div className="flex justify-center items-center">
-    //             <Typography fontWeight="medium" fontSize="h3">
-    //               {`${totalCondidate}`}
-    //               {/* Dynamically display the count of CardUser */}
-    //             </Typography>
-    //             <Typography fontSize="h5" color="grey" className="mt-1">
-    //               Candidates
-    //             </Typography>
-    //           </div>
-    //         </div>
-    //         {/* table accepted */}
-    //         <div className="w-[250px] h-[70px] mb-14 rounded-[10px] border-[#E8E8E8] border-[0.5px] flex justify-center items-center gap-2 shadow-md ">
-    //           <Typography color="green" fontSize="h3" fontWeight="medium">
-    //             Accepted :
-    //           </Typography>
-    //           <div className="flex justify-center items-center">
-    //             <Typography fontWeight="medium" fontSize="h3">
-    //               130
-    //             </Typography>
-    //             <Typography fontSize="h5" color="grey" className="mt-1">
-    //               Candidates
-    //             </Typography>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-
-    //   <div className="w-[1024px] mx-auto mt-10">
-    //     <table className="table-auto w-full">
-    //   <thead>
-    //     <tr className="bg-white border-[#E8E8E8] h-[50px]">
-    //       <th
-    //         colSpan={5}
-    //         className="text-start font pl-7 rounded-t-lg border-[#E8E8E8]"
-    //       >
-    //         Cambodia Book Fair
-    //       </th>
-    //     </tr>
-    //     <tr>
-    //       <th className="h-[50px] border-[#E8E8E8]"></th>
-    //       <th className="h-[50px] border-[#E8E8E8]">Name</th>
-    //       <th className="h-[50px] border-[#E8E8E8]">Date</th>
-    //       <th className="h-[50px] border-[#E8E8E8]">Email</th>
-    //       <th className="h-[50px] border-[#E8E8E8]"></th>
-    //     </tr>
-    //   </thead>
-    //   <tbody className="text-center bg-white">
-    //     {CardUser.map((item, index) => (
-    //       <tr key={index}>
-    //         <td>
-    //           <div className="flex justify-center items-center">
-    //             <Image
-    //               src={item.profile}
-    //               alt=""
-    //               width={60}
-    //               height={60}
-    //             ></Image>
-    //           </div>
-    //         </td>
-    //         <td>{item.name}</td>
-    //         <td>{item.date}</td>
-    //         <td>{item.gmail}</td>
-    //         <td>
-    //           <div className="flex items-center justify-center gap-4">
-    //             <Button className="flex items-center hover:[#BDD8FF] w-[60px] h-[40px] rounded-[15px] text-center justify-center bg-[#E8E8E8] !text-[16px]">
-    //               View
-    //             </Button>
-    //             <div>
-    //               <button className="rounded-full w-[23px] h-[23px] bg-green-500 flex justify-center items-center"></button>
-    //             </div>
-    //           </div>
-    //         </td>
-    //       </tr>
-    //     ))}
-    //   </tbody>
-    // </table>
-    //   </div>
-    // </div>
+ 
     <div className="bg-[#FAFAFA]">
       <div className="ml-[121px] mr-[128px]">
         {/* Header section */}
@@ -187,7 +97,7 @@ const Page = () => {
                   </td>
                   <td>{item.name}</td>
                   <td>{item.gmail}</td>
-                  <td className="text-gray-500">{item.status}</td>
+                  <td className={`${item.status === "Accepted" ? "text-green-500": item.status === "Rejected"? "text-red-500": "text-gray-500"}`}>{item.status}</td>
                   <td>{item.date}</td>
                   <td>
                       <Button className="flex items-center hover:bg-[#207BFF] w-[50px] h-[23px] rounded-[15px] text-center justify-center bg-white hover:text-white !text-[10px] text-[#757575]">
